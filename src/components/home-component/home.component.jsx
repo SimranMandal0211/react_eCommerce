46adb66ac6fa9b881caf53b8fbd8abcdfa7c9ec3
Simran Mandal <simranmandal0211@gmail.com>
2023-11-13 16:57:51 +0530

    add & style:: sort page and home component

--- src/components/home-component/home.component.jsx
+++ src/components/home-component/home.component.jsx
@@ -1,10 +1,11 @@
 import Products from '../products/products.component';
+import Sort from '../Sort/sort.component';
 import './homeComp.styles.css';
 
 export default function HomeComponent(){
     return (
         <div className="home-container">
-            <p>Sort</p>
+            <Sort />
             <Products />
         </div>
     );
---
+++ src/components/sort/sort.component.jsx
@@ -0,0 +1,9 @@
+import './sort.styles.css';
+
+export default function Sort(){
+    return (
+        <div className="sort-btn">
+            <p>X Sort by Price</p>
+        </div>
+    )
+}
